265cfc063c7524ffebc998a659bd1850e0c20dc9
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-07-01 16:43:09 -0500

    Load Decoded Image from Memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 use base64::decode as b64_decode;
+use image::load_from_memory;
 
 #[cfg(test)]
 mod tests {
@@ -17,4 +18,7 @@ pub fn grayscale(encoded_file: &str) {
 
     let base64_to_vector = b64_decode(encoded_file).unwrap();
     log(&"Image decoded".into());
+
+    let image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
