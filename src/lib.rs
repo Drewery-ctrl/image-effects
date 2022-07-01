a15e860292c50cfda711906b0ed870473274a856
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-07-01 16:30:59 -0500

    Pass Base64 File from JS to Rust & Log it via Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::console::log_1 as log;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,4 +8,9 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+}
+
+#[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) {
+    log(&encoded_file.into());
 }
